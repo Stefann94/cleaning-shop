4cf914e10bc40a85c0815694c48373bceae64ad3
Stefann94 <stefancozma94@gmail.com>
2026-02-11 00:02:19 +0200

    inceput baie

---
+++ componente.js
@@ -0,0 +1,126 @@
+// componente.js
+document.addEventListener("DOMContentLoaded", () => {
+    // Detectăm dacă suntem într-un subfolder pentru a ajusta căile (../)
+    const isSubpage = window.location.pathname.includes('/') && 
+                     !window.location.pathname.endsWith('index.html') && 
+                     window.location.pathname.split('/').length > 2;
+    
+    const pathPrefix = isSubpage ? "../" : "";
+
+    const headerHTML = `
+    <nav class="navbar">
+        <div class="nav-container">
+            <div class="burger-menu" id="mobile-menu">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+
+            <div class="logo">
+                <a href="${pathPrefix}index.html">CLEAN<span>SHOP</span></a>
+            </div>
+
+            <ul class="nav-links">
+                <li><a href="${pathPrefix}index.html">Home</a></li>
+                
+                <li class="has-dropdown">
+                    <a href="${pathPrefix}baie/baie.html">Baie <img src="${pathPrefix}pictures/baie_btn.png" class="nav-icon" alt=""></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Detergenți WC</a></li>
+                        <li><a href="#">Anticalcar</a></li>
+                        <li><a href="#">Odorizante</a></li>
+                    </ul>
+                </li>
+
+                <li class="has-dropdown">
+                    <a href="${pathPrefix}bucatarie/bucatarie.html">Bucătărie <img src="${pathPrefix}pictures/bucatarie_btn.png" class="nav-icon" alt=""></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Degresanți</a></li>
+                        <li><a href="#">Detergenți Vase</a></li>
+                        <li><a href="#">Curățare Cuptor</a></li>
+                    </ul>
+                </li>
+
+                <li class="has-dropdown">
+                    <a href="${pathPrefix}pardoseli/pardoseli.html">Pardoseli <img src="${pathPrefix}pictures/pardoseli.png" class="nav-icon" alt=""></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Detergent Parchet</a></li>
+                        <li><a href="#">Soluții Gresie</a></li>
+                        <li><a href="#">Mopuri & Găleți</a></li>
+                    </ul>
+                </li>
+
+                <li class="has-dropdown">
+                    <a href="${pathPrefix}rufe/rufe.html">Rufe <img src="${pathPrefix}pictures/rufe.png" class="nav-icon" alt=""></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Detergent Lichid</a></li>
+                        <li><a href="#">Balsam Rufe</a></li>
+                        <li><a href="#">Îndepărtare Pete</a></li>
+                    </ul>
+                </li>
+
+                <li class="has-dropdown">
+                    <a href="${pathPrefix}igiena/igiena.html">Igienă <img src="${pathPrefix}pictures/igiena_btn.png" class="nav-icon" alt=""></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Săpun Lichid</a></li>
+                        <li><a href="#">Dezinfectanți Mâini</a></li>
+                        <li><a href="#">Gel de Duș</a></li>
+                    </ul>
+                </li>
+
+                <li class="has-dropdown">
+                    <a href="${pathPrefix}accesorii/accesorii.html">Accesorii <img src="${pathPrefix}pictures/accesorii.png" class="nav-icon" alt=""></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Lavete & Bureți</a></li>
+                        <li><a href="#">Mănuși Protecție</a></li>
+                        <li><a href="#">Perii Curățenie</a></li>
+                    </ul>
+                </li>
+
+                <li><a href="${pathPrefix}contact/contact.html">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="topbar">
+        <div class="topbar-container">
+            <div class="top-search">
+                <input type="text" placeholder="Caută produse">
+                <button class="search-btn">
+                    <img src="${pathPrefix}pictures/search-icon.png" alt="Search">
+                </button>
+            </div>
+
+            <div class="topbar-items">
+                <div class="top-item">
+                    <img src="${pathPrefix}pictures/phone-icon.png" class="top-icon" alt="">
+                    <span>07xx xxx xxx</span>
+                </div>
+                <div class="top-item">
+                    <img src="${pathPrefix}pictures/pin-icon.png" class="top-icon" alt="">
+                    <span>Locație: București</span>
+                </div>
+                <div class="top-auth">
+                    <a href="#" class="auth-link login">Log In</a>
+                    <span class="auth-divider"></span>
+                    <a href="#" class="auth-link signup">Sign Up</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+
+    // Inserăm codul la începutul body-ului
+    document.body.insertAdjacentHTML("afterbegin", headerHTML);
+
+    // Re-atașăm logica de burger menu deoarece elementul a fost creat dinamic
+    const burger = document.querySelector('#mobile-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    if (burger) {
+        burger.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+            burger.classList.toggle('is-active');
+        });
+    }
+});
