9c71f80aa490ba59d10215b7cada3cfda7759f32
Stefann94 <stefancozma94@gmail.com>
2026-02-11 18:37:26 +0200

    configurare supabase si vita3

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        baie: resolve(__dirname, 'baie/baie.html'),
+        // Adaugă aici restul paginilor pe măsură ce le faci:
+        // bucatarie: resolve(__dirname, 'bucatarie/bucatarie.html'),
+      },
+    },
+  },
+});
