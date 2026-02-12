41b5228808dfd1d976cd64075398d176295f7379
Stefann94 <stefancozma94@gmail.com>
2026-02-12 17:35:58 +0200

    rufe

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
         baie: resolve(__dirname, 'baie/baie.html'),
         login: resolve(__dirname, 'login/login.html'),
         bucatarie: resolve(__dirname, 'bucatarie/bucatarie.html'),
-        pardoseli: resolve(__dirname, 'pardoseli/pardoseli.html')
+        pardoseli: resolve(__dirname, 'pardoseli/pardoseli.html'),
+        rufe: resolve(__dirname, 'rufe/rufe.html'),
         
         // Adaugă aici restul paginilor pe măsură ce le faci:
         // bucatarie: resolve(__dirname, 'bucatarie/bucatarie.html'),
